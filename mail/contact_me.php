e7f3d864d8084490225afef882e0032979826a15
genixers <hossein.ghodsi@genixers.com>
2017-02-13 13:10:05 -0600

    testing email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,5 +1,6 @@
 <?php
 // Check for empty fields
+/*
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
    empty($_POST['phone']) 		||
@@ -23,4 +24,5 @@ $headers = "From: noreply@yourdomain.com\n"; // This is the email address the ge
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
+*/
 ?>
